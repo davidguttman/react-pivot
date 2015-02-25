11f5703316bd2760063db70cac32c487d05ce3ee
David Guttman <david@davidguttman.com>
2015-02-25 12:42:18 -0800

    first stab at dimensions

--- example/index.jsx
+++ example/index.jsx
@@ -1,7 +1,21 @@
 var React = require('react')
 var ReactPivot = require('..')
 
+var data = require('./data.json')
+
+var dimensions = [
+  {property: 'firstName', title: 'First Name'},
+  {property: 'lastName', title: 'Last Name'},
+  {property: 'state', title: 'State'},
+  {property: function(row) {
+    return row.transaction.business
+  }, title: 'Business'},
+  {property: function(row) {
+    return row.transaction.type
+  }, title: 'Transaction Type'}
+]
+
 React.render(
-  <ReactPivot />,
+  <ReactPivot rows={data} dimensions={dimensions}/>,
   document.body
 )
--- index.jsx
+++ index.jsx
@@ -1,12 +1,92 @@
-var React = require('React')
+var _ = require('underscore')
+var React = require('react')
+
 module.exports = React.createClass({
   getInitialState: function() {
-    return {}
+    console.log('this.props', this.props)
+    return {
+      dimensions: [],
+      calculations: {}
+    }
+  },
+
+  toggleDimension: function (evt) {
+    var dimension = evt.target.value
+
+    var dimensions = this.state.dimensions
+    if (_.contains(dimensions, dimension)) {
+      this.setState({dimensions: _.without(dimensions, dimension)})
+    } else {
+      dimensions.push(dimension)
+      this.setState({dimensions: dimensions})
+    }
   },
 
   render: function() {
+    var self = this
+
     return (
-      <h1>ReactPivot!</h1>
+      <div>
+        <h1>ReactPivot!</h1>
+
+        <div className="dimensions">
+          {this.props.dimensions.map(function(dimension) {
+            var checked = _.contains(self.state.dimensions, dimension.title)
+
+            return (
+              <label>
+                <input type='checkbox'
+                       value={dimension.title}
+                       checked={checked}
+                       onChange={self.toggleDimension} />
+
+                {dimension.title}
+              </label>
+            )
+          })}
+        </div>
+
+        <div className="calculations">
+          Calculations
+        </div>
+
+        <div className="results">
+          <table>
+            <thead>
+              <tr>
+                { self.state.dimensions.map(function(dTitle) {
+                  return (
+                    <th>{dTitle}</th>
+                  )
+                })}
+              </tr>
+            </thead>
+            <tbody>
+              {this.props.rows.map(function(row) {
+                return (
+                  <tr>
+                    { self.state.dimensions.map(function(dTitle) {
+                      var dimension = _.find(self.props.dimensions, function(d) {
+                        return d.title === dTitle
+                      })
+                      var val
+                      if (typeof dimension.property === 'string') {
+                        val = row[dimension.property]
+                      } else {
+                        val = dimension.property(row)
+                      }
+                      return(
+                        <td>{val}</td>
+                      )
+                    }) }
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+
+      </div>
     )
   }
 })
