cc669ae728a24702113a87a3d58adab62ab93870
David Guttman <david@davidguttman.com>
2015-02-25 10:17:09 -0800

    baseline

---
+++ example/index.jsx
@@ -0,0 +1,7 @@
+var React = require('react')
+var ReactPivot = require('..')
+
+React.render(
+  <ReactPivot />,
+  document.body
+)
---
+++ index.jsx
@@ -0,0 +1,12 @@
+var React = require('React')
+module.exports = React.createClass({
+  getInitialState: function() {
+    return {}
+  },
+
+  render: function() {
+    return (
+      <h1>ReactPivot!</h1>
+    )
+  }
+})
